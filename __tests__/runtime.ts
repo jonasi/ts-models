71f570524756b92ad18d7b323ced69d1aa58de4c
Isao Jonas <isao.jonas@gmail.com>
2020-05-27 16:08:24 -0500

    more tests and use try/catch

--- __tests__/runtime.ts
+++ __tests__/runtime.ts
@@ -1,33 +1,45 @@
-import { assert, checkShapeOf, checkString, checkNumber, JSONValue, Check, checkDate } from '../src/index';
+import { assert, checkShapeOf, checkString, checkNumber, JSONValue, Check, checkDate, checkArray, checkArrayOf, CheckError } from '../src/index';
 
-const valueTests: { type: string, it: string, input: JSONValue | undefined, check: Check<unknown>, output?: unknown, error?: boolean }[] = [
+const valueTests: { type: string, it: string, input: JSONValue | undefined, check: Check<unknown>, output?: unknown, error?: string }[] = [
     { type: 'string', it: 'should pass through', check: checkString, input: 'str', output: 'str' },
     { type: 'string', it: 'should default to empty string (undefined)', check: checkString, input: void 0, output: '' },
     { type: 'string', it: 'should default to empty string (null)', check: checkString, input: null, output: '' },
-    { type: 'string', it: 'should throw on non-string', check: checkString, input: false, error: true },
+    { type: 'string', it: 'should throw on non-string', check: checkString, input: false, error: '' },
     { type: 'number', it: 'should pass through', check: checkNumber, input: 4, output: 4 },
-    { type: 'number', it: 'should throw on non-number', check: checkNumber, input: '4', error: true },
+    { type: 'number', it: 'should throw on non-number', check: checkNumber, input: '4', error: '' },
     { type: 'date', it: 'should handle numbers', check: checkDate, input: 1590541512706, output: new Date(1590541512706) },
     { type: 'date', it: 'should handle strings', check: checkDate, input: 'Tue May 26 2020 20:05:12 GMT-0500', output: new Date('Tue May 26 2020 20:05:12 GMT-0500') },
-    { type: 'date', it: 'should throw on non-numbers or non-strings', check: checkDate, input: {}, error: true },
+    { type: 'date', it: 'should throw on non-numbers or non-strings', check: checkDate, input: {}, error: '' },
+    { type: 'array', it: 'should default to empty array (undefined)', check: checkArray, input: void 0, output: [] },
+    { type: 'array', it: 'should default to empty array (null)', check: checkArray, input: null, output: [] },
+    { type: 'array', it: 'should handle empty array', check: checkArray, input: [], output: [] },
+    { type: 'shapeOf', it: 'should handle empty object', check: checkShapeOf({}), input: {}, output: {} },
+    { type: 'shapeOf', it: 'should throw on null', check: checkShapeOf({}), input: null, error: '' },
+    { type: 'shapeOf', it: 'should throw on undefined', check: checkShapeOf({}), input: void 0, error: '' },
+    { type: 'shapeOf', it: 'should skip extra data', check: checkShapeOf({}), input: { a: 'string' }, output: {} },
+    { type: 'shapeOf', it: 'should throw on missing data', check: checkShapeOf({ a: checkNumber }), input: { }, error: '.a' },
+    { type: 'shapeOf', it: 'should accept missing data for string', check: checkShapeOf({ a: checkString }), input: { }, output: { a: '' } },
+    { type: 'arrayOf', it: 'should handle an empty array', check: checkArrayOf(checkString), input: [], output: [] },
+    { type: 'arrayOf', it: 'should default to an empty array (undefined)', check: checkArrayOf(checkString), input: void 0, output: [] },
+    { type: 'arrayOf', it: 'should default to an empty array (null)', check: checkArrayOf(checkString), input: null, output: [] },
+    { type: 'arrayOf', it: 'should work (strings)', check: checkArrayOf(checkString), input: [ 'str', 'str' ], output: [ 'str', 'str' ] },
+    { type: 'arrayOf', it: 'should throw (strings)', check: checkArrayOf(checkString), input: [ 3, 'str' ], error: '[0]' },
 ];
 
 valueTests.forEach(t => {
     describe(t.type, () => {
         it(t.it, () => {
-            if (t.error) {
-                expect(() => assert(t.input, t.check)).toThrow();
+            if (typeof t.error === 'string') {
+                let err: Error | undefined;
+                try {
+                    assert(t.input, t.check);
+                } catch(e) { err = e; }
+
+                expect(err).toBeInstanceOf(CheckError);
+                expect((err as CheckError).path).toBe(t.error);
             } else {
                 expect(assert(t.input, t.check)).toEqual(t.output);
             }
         });
-    });
-});
-
-describe('shape', () => {
-    it('should only include listed properties', () => {
-        expect(assert({ a: 'string', b: 'string' }, checkShapeOf({
-            a: checkString,
-        }))).toEqual({ a: 'string' });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,30 @@
 export type JSONValue = undefined | null | string | number | boolean | JSONValue[] | { [prop: string]: JSONValue };
 
 export interface Check<T> {
-    (js: JSONValue): [ T ] | undefined;
+    (js: JSONValue, path: string): T;
     id: string;
+}
+
+export class CheckError extends Error {
+    path: string;
+    constructor(message: string, path: string) {
+        message = `Check error at ${ path }: ${ message }`;
+        super(message);
+        this.path = path;
+
+        // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+        Object.setPrototypeOf(this, CheckError.prototype);
+    }
 }
 
 type CheckArr<T extends [ unknown ] | unknown[]> = { [P in keyof T]: Check<T[P]> };
 
 export function assert<T>(js: JSONValue, check: Check<T>): T {
-    const v = check(js);
-    if (v) {
-        return v[0];
-    }
-
-    throw new Error("Value failed " + check.id);
+    return check(js, '');
 }
 
-function makeCheck<T>(id: string, fn: (js: JSONValue) => [ T ] | undefined, def?: T): Check<T> {
-    const ch = (js: JSONValue): [ T ] | undefined => {
-        const v = fn(js);
-        if (v) {
-            return v;
-        }
-
-        if (isEmpty(js) && def !== void 0) {
-            return [ def ];
-        }
-    };
-
+function makeCheck<T>(id: string, fn: (js: JSONValue, path: string) => T): Check<T> {
+    const ch = (js: JSONValue, path: string): T => fn(js, path);
     ch.id = id;
 
     return ch;
@@ -38,25 +35,78 @@ function isEmpty(v: unknown): boolean {
     return v === void 0 || v === null;
 }
 
-export const checkEmpty = makeCheck('empty', js => isEmpty(js) ? [ void 0 ] : void 0);
-export const checkString = makeCheck('string', js => typeof js === 'string' ? [ js ] : void 0, '');
-export const checkBoolean = makeCheck('boolean', js => typeof js === 'boolean' ? [ js ] : void 0);
-export const checkNumber = makeCheck('number', js => typeof js === 'number' ? [ js ] : void 0);
-export const checkObject = makeCheck('object', js => typeof js === 'object' && js && !Array.isArray(js) ? [ js ] : void 0);
-export const checkArray = makeCheck('array', js => Array.isArray(js) ? [ js ] : void 0, []);
-export const checkDate = makeCheck('date', js => typeof js === 'string' || typeof js === 'number' ? [ new Date(js) ] : void 0);
+export const checkEmpty = makeCheck('empty', (js, path) => {
+    if (isEmpty(js)) {
+        return void 0;
+    }
+
+    throw new CheckError('Expected null or undefined and found ' + (typeof js), path);
+});
+
+export const checkString = makeCheck('string', (js, path) => {
+    if (typeof js === 'string') {
+        return js;
+    } else if (isEmpty(js)) {
+        return '';
+    }
+
+    throw new CheckError('Expected a string and found ' + (typeof js), path);
+});
+
+export const checkBoolean = makeCheck('boolean', (js, path) => {
+    if (typeof js === 'boolean') {
+        return js;
+    }
+
+    throw new CheckError('Expected a boolean and found ' + (typeof js), path);
+});
+
+export const checkNumber = makeCheck('number', (js, path) => {
+    if (typeof js === 'number') {
+        return js;
+    }
+
+    throw new CheckError('Expected a number and found ' + (typeof js), path);
+});
+
+export const checkObject = makeCheck('object', (js, path) => {
+    if (typeof js === 'object' && js && !Array.isArray(js)) {
+        return js;
+    }
+
+    throw new CheckError('Expected an object and found ' + (typeof js), path);
+});
+
+export const checkArray = makeCheck('array', (js, path) => {
+    if (Array.isArray(js)) {
+        return js;
+    } else if (isEmpty(js)) {
+        return [];
+    }
+
+    throw new CheckError('Expected an array and found ' + (typeof js), path);
+});
+
+export const checkDate = makeCheck('date', (js, path) => {
+    if (typeof js === 'string' || typeof js === 'number') {
+        return new Date(js);
+    }
+
+    throw new CheckError('Expected a string or number and found ' + (typeof js), path);
+});
 
 export function checkOr<T extends [ unknown ] | unknown[]>(checks: CheckArr<T>): Check<T[number]> {
     // eslint-disable-next-line
     // @ts-ignore
     const id = 'or(' + checks.map(c => c.id).join(', ') + ')';
-    return makeCheck(id, js => {
+    return makeCheck(id, (js, path) => {
         for (const i in checks) {
-            const v = checks[i](js);
-            if (v) {
-                return v;
-            }
+            try {
+                return checks[i](js, path);
+            } catch (err) {}
         }
+
+        throw new CheckError('No check matches or condition', path);
     });
 }
 
@@ -68,71 +118,45 @@ export function checkAnd<T extends [ unknown ] | unknown[]>(checks: CheckArr<T>)
     // eslint-disable-next-line
     // @ts-ignore
     const id = 'and(' + checks.map(c => c.id).join(', ') + ')';
-    return makeCheck(id, js => {
+    return makeCheck(id, (js, path) => {
         const obj = {};
         for (const i in checks) {
-            const v = checks[i](js);
-            if (!v) {
-                return v;
-            }
-
-            Object.assign(obj, v[0]);
+            const v = checks[i](js, path);
+            Object.assign(obj, v);
         }
 
-        return [ obj as UnionToIntersection<T[number]> ];
+        return obj as UnionToIntersection<T[number]>;
     });
 }
 
 export function checkArrayOf<T>(check: Check<T>): Check<T[]> {
     const id = 'arrayOf(' + check.id + ')';
-    return makeCheck(id, js => {
-        const arr = checkArray(js);
-        if (!arr) {
-            return;
-        }
-
-        const ret = [];
-        for (const v of arr) {
-            const checked = check(v);
-            if (!checked) {
-                return;
-            }
-            ret.push(checked[0]);
-        }
-
-        return [ ret ];
-    });
+    return makeCheck(id, (js, path) => checkArray(js, path).map((item, i) => (
+        check(item, `${ path }[${ i }]`)
+    )));
 }
 
 type ShapeCheck<T extends Record<string, unknown>> = { [P in keyof T]: Check<T[P]> };
 
 export function checkShapeOf<T extends Record<string, unknown>>(checks: ShapeCheck<T>): Check<T> {
-    return makeCheck('shapeOf', js => {
-        const obj = checkObject(js);
-        if (!obj) {
-            return void 0;
-        }
-
+    return makeCheck('shapeOf', (js, path) => {
+        const obj = checkObject(js, path);
         const ret: Partial<T> = {};
         for (const k in checks) {
-            const v = checks[k](obj[0][k]);
-            if (!v) {
-                return;
-            }
-
-            ret[k] = v[0];
+            const v = checks[k](obj[k], `${ path }.${ k }`);
+            ret[k] = v;
         }
 
-        return [ ret ] as [ T ];
+        return ret as T;
     });
 }
 
 export function checkLiteralOf<T extends JSONValue>(v: T): Check<T> {
-    return makeCheck(`literalOf(${ v })`, js => {
+    return makeCheck(`literalOf(${ v })`, (js, path) => {
         if (js === v) {
-            return [ js ] as [ T ];
+            return js as T; 
         }
 
-        return;
+        throw new CheckError(`Expected ${ v } and found ${ js }`, path);
     });
 }
